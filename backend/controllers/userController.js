fe236f62e29917329107dc1e9f092db651fc78ee
Rajeev <ronaldocrcr5@gmail.com>
2024-06-27 18:52:42 +0530

    implemented login api and sents a response successfully

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 import generateTokenAndSetCookie from "../util/helpers/generateTokenAndSetCookie.js";
 
+// Sign-Up User
 const signupUser = async (req, res) => {
   try {
     const { name, email, username, password } = req.body;
@@ -31,6 +32,7 @@ const signupUser = async (req, res) => {
     if (newUser) {
       generateTokenAndSetCookie(newUser._id, res);
 
+      //sending the response
       res.status(201).json({
         _id: newUser._id,
         name: newUser.name,
@@ -48,4 +50,35 @@ const signupUser = async (req, res) => {
   }
 };
 
-export default signupUser;
+// Login User
+const loginUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+
+    // check if password is correct
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+
+    // check if user or password is valid
+    if (!user || !isPasswordCorrect)
+      return res.status(400).json({ error: "Invalid username & password" });
+
+    generateTokenAndSetCookie(user._id, res);
+
+    //sending the response
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.name,
+      username: user.username,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    console.log("Error in login user: ", error.message);
+  }
+};
+
+export { signupUser, loginUser };
--- backend/util/helpers/generateTokenAndSetCookie.js
+++ backend/util/helpers/generateTokenAndSetCookie.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 const generateTokenAndSetCookie = (userId, res) => {
-  // create the token
+  // create the token using Refresh tokens
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "15d",
   });
@@ -15,3 +15,11 @@ const generateTokenAndSetCookie = (userId, res) => {
 };
 
 export default generateTokenAndSetCookie;
+
+// "Access tokens" and "Refresh tokens" are both used in authentication and authorization processes in JSON Web Tokens (JWTs):
+
+// Access tokens
+// These temporary credentials grant users access to protected resources, such as files owned by others, or shared resources. They contain information about the user or device, and are usually short-lived. Access tokens can be used for passwordless authentication solutions, or to authenticate API requests.
+
+// Refresh tokens
+// These tokens extend the lifespan of access tokens by allowing users to obtain new access tokens when the current ones expire. Refresh tokens are stored as HTTP-only cookies, and can be used to re-authenticate users without exposing sensitive information to JavaScript. This is useful in scenarios where users aren't always available to provide credentials again and again.
