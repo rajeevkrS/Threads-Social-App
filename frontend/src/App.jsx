ccbacaf710a21404b0e570d8ffb477dcc4e776ed
Rajeev <ronaldocrcr5@gmail.com>
2024-07-06 15:05:12 +0530

    successfully made signup and login card using chakra ui and installed @chakra/icons and recoil for state management

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,17 @@ import { Routes, Route } from "react-router-dom";
 import Headers from "./components/Header";
 import PostPage from "./pages/PostPage";
 import UserPage from "./pages/UserPage";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <Container maxW="620px">
       <Headers />
       <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={<AuthPage />} />
+
         <Route path="/:username" element={<UserPage />} />
         <Route path="/:username/post/:pid" element={<PostPage />} />
       </Routes>
---
+++ frontend/src/pages/AuthPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SignUp from "../components/SignUp";
+import Login from "../components/Login";
+
+const AuthPage = () => {
+  return (
+    <>
+      {/* <SignUp /> */}
+      <Login />
+    </>
+  );
+};
+
+export default AuthPage;
