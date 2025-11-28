b23da23ad78607d8738546cfda12f8497c541d2a
Rajeev Kumar Sudhansu <rajeevkumarr1221@gmail.com>
2025-11-28 23:49:13 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-import path from "path";
+// import path from "path";
 import express from "express";
 import { connectDB } from "./db/connectDB.js";
 import cookieParser from "cookie-parser";
@@ -19,7 +19,7 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 // cloudinary configuration
 cloudinary.config({
@@ -41,21 +41,21 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/messages", messageRoutes);
 
-// app.get("/", (req, res) => {
-//   res.send("API Working");
-// });
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
 
 // Merge- http://localhost:5000 => backend + frontend
 // Serve static assets only if in production
-if (process.env.NODE_ENV == "production") {
-  // run the middleware
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// if (process.env.NODE_ENV == "production") {
+//   // run the middleware
+//   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  // react app
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
+//   // react app
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+//   });
+// }
 
 server.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`);
